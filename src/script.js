d6db93b2fad92b3cf255a398c995f47ef2c62e01
Daniel Wear <daniel@sdninc.net>
2020-03-30 17:51:50 -0700

    Web Based Modern Word Processor

---
+++ src/script.js
@@ -0,0 +1,21 @@
+DecoupledEditor
+    .create( document.querySelector( '.document-editor__editable' ), 
+           {
+        cloudServices: {
+            tokenUrl: '',
+            uploadUrl: 'https://your-organization-id.cke-cs.com/easyimage/upload/'
+        }
+    }
+           )
+    .then( editor => {
+        const toolbarContainer = document.querySelector( '.document-editor__toolbar' );
+
+        toolbarContainer.appendChild( editor.ui.view.toolbar.element );
+
+        window.editor = editor;
+    } )
+    .catch( err => {
+        console.error( err );
+    } );
+
+
